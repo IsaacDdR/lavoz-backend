eaa2aaef790c048375a0bd250336abf2274cc239
Isaacddr <isaacovic00@gmail.com>
2021-07-02 22:18:09 -0500

    Added background

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
+  admin: {
+    auth: {
+      secret: env("ADMIN_JWT_SECRET", "236261919babb647bb83c7eab29a9a77"),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'cfb78b03-9204-4ffe-92d6-378c9717c25c'
+};
